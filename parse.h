40917261b7a0beef2ffbadf936ddfeeea3c54ea6
gridstop <gridstop@gmail.com>
2012-10-01 19:20:00 -0500

    Added test 1 to readme

--- parse.h
+++ parse.h
@@ -3,7 +3,8 @@
 
 #include <stdio.h>
 
-// The only publicly visible function for parse.c
+// Parses and issues the jobs for a single line from a given file handle.
+// Returns 0 if not EOF, or -1 on EOF
 int parse_line(FILE *fp);
 
 #endif
